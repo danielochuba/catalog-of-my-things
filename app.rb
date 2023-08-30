894dbc921cf2712c4bcb9c17ffe5dd64fdb2a33d
Daniel Ochuba <danielochuba78@gmail.com>
2023-08-30 16:17:22 +0100

    import classes and create App class in app.rb

---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'date'
+require './classes/item'
+require './classes/genre'
+require './classes/author'
+require './classes/book'
+require './classes/music_album'
+require './classes/game'
+require './classes/label'
+
+
+class App
+    def initialize
+        @genres = []
+        @books = []
+        @music_albums = []
+        @games = []
+        @authors = []
+        @labels = []
+        @items = []        
+    end
+
+
+end
