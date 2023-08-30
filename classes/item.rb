17e589a5fd9823b38c469a05a73cf73659656c83
Daniel Ochuba <danielochuba78@gmail.com>
2023-08-30 09:42:45 +0100

    modify item class relationships

--- classes/item.rb
+++ classes/item.rb
@@ -1,4 +1,7 @@
 require 'date'
+require_relative 'genre'
+require_relative 'author'
+require_relative 'label'
 
 class Item
 
@@ -6,27 +9,35 @@ class Item
     attr_accessor :archived
     def initialize(genre, author, label, date)
         @id = Random.rand(1..100)  
+        
         @genre = genre
+        genre.items << self
+
         @author = author
+        author.items << self
+
         @label = label
+        label.items << self
+
         @publish_date = date
+
         @archived = false
     end
 
-    def genre=(genre)
-        @genre = genre
-        genre.add_item(self) unless genre.items.include?(self)
-    end
+    # def genre=(genre)
+    #     @genre = genre
+    #     genre.add_item(self) unless genre.items.include?(self)
+    # end
 
-    def author=(author)
-        @author = author
-        author.add_item(self) unless author.items.include?(self)
-    end
+    # def author=(author)
+    #     @author = author
+    #     author.add_item(self) unless author.items.include?(self)
+    # end
 
-    def label=(label)
-        @label = label
-        label.add_item(self) unless label.items.include?(self)
-    end
+    # def label=(label)
+    #     @label = label
+    #     label.add_item(self) unless label.items.include?(self)
+    # end
 
     def move_to_archive
         @archived = true if can_be_archived?
