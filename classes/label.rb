a36d9e8cae9d9d88dfdd66d0322632e9edce4254
Daniel Ochuba <danielochuba78@gmail.com>
2023-08-30 01:23:24 +0100

    add both ways relationship in Items

--- classes/label.rb
+++ classes/label.rb
@@ -1,4 +1,6 @@
 class Label
+    attr_accessor :id, :title, :color
+    attr_reader :items
     def initialize(title, color)
         @id = Random.rand(301..400)
         @title = title
@@ -9,5 +11,7 @@ class Label
     def add_item(item)
         @items << item
         item.label = self
+
+        item
     end
 end
