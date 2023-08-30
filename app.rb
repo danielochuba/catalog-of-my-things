e21aff9cbfd625b4885cb1b3f1e1b4ffcfd89fe9
Daniel Ochuba <danielochuba78@gmail.com>
2023-08-30 16:21:43 +0100

    add display_options method in app.rb

--- app.rb
+++ app.rb
@@ -19,5 +19,23 @@ class App
         @items = []        
     end
 
+    def display_options
+        
+        puts "Please select an option:"
+        puts "  1. Create a new book"
+        puts "  2. Create a new music album"
+        puts "  3. Create a new game"
+        puts "  4. List all books"
+        puts "  5. List all music albums"
+        puts "  6. List all games"
+        puts "  7. List all genres"
+        puts "  8. List all labels"
+        puts "  9. List all authors"
+        puts " 10. List all items"
+        puts " 11. Archive an item"
+        puts " 12. Exit"
+        puts
+    end
+
 
 end
